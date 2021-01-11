e4e808870e399b0306e6914918aa1bb8cf5cc766
Jessica Lynne Byrne <Jessica@Jessicas-MacBook-Air.local>
2021-01-10 20:12:59 -0700

    WIP: typing out proxy

---
+++ server/proxy.mjs
@@ -0,0 +1,49 @@
+import * as dotenv from 'dotenv';
+
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import axios from 'axios';
+import { AxiosResponse, AxiosError } from 'axios';
+
+// initialize configuration
+dotenv.config();
+
+const app: Application = express();
+
+app.use(morgan('tiny')); // logging
+app.use(cors({ credentials: true, origin: true }));
+
+app.get('/v1/*', (req: Request, res: Response) => {
+  // Possible improvement: parse url data
+  let url = `https://pro-api.coinmarketcap.com${req.originalUrl}`;
+
+  axios
+    .get(url, {
+      headers: {
+        'X-CMC_PRO_API_KEY': process.env.CMC_API_KEY,
+        Accept: 'application/json',
+        'Access-Control-Allow-Origin': 'http://localhost:3000',
+        'Access-Control-Allow-Credentials': true,
+      },
+    })
+    .then((response: AxiosResponse) => {
+      const data = response['data'].data;
+      const cryptoType = Object.keys(data)[0];
+      const price = data[cryptoType].quote.USD.price;
+      res.json({ price: price });
+    })
+    .catch((error: AxiosError) => {
+      if (error.response) {
+        return res
+          .status(error.response.status)
+          .send(error.response.statusText);
+      }
+      res.send(error.message);
+    });
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log('Listening on port ', port);
+});
